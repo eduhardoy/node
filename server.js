4f05e5da930b8b2cc229ed5fc06b37db365d5f41
Edu Hardoy <eduhardoy@gmail.com>
2021-01-21 20:14:09 -0300

    0.2

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const response = require ('./network/response');
 
 const router = express.Router();
 
+const chalk = require('chalk');
 
 var app = express();
 
@@ -25,7 +26,7 @@ router.post('/message', function(req, res) {
   console.log(req.query); 
   console.log(req.body); 
   if (req.query.error == "ok") {
-    response.error(req, res, "Error simulado", 400)
+    response.error(req, res, "Error inesperado", 500, "Es solo una simulacion de los errores")
   } else {
     response.success(req, res, "Creado correctamente", 201)
   }
